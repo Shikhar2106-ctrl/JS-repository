4dd2bb0950ccc01c00ceaa18c18d1e4849222f9b
Shikhar2106 <141298713+Shikhar2106-ctrl@users.noreply.github.com>
2024-03-08 10:37:14 +0000

    Pending changes exported from your codespace

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+console.log("shikhar");
+const accountId= 4356256
+let email= "shikharseth21-78@gmail.com"
+var accountPassword= "4536632"
+accountCity = "Jaipur"
+let accountState;
+//accountId= 5632776   ---> can't be changed as it was declared as const.
+console.log(accountCity)
+accountPassword= "6326567"
+email= "vdsgdfdsbd bv@"
+console.table([accountId, email, accountPassword, accountCity])
+/* 
+prefer not to use var keyword
+due to issue in block scope  and functional scope 
+*/
